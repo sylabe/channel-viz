6eb1be39124a65ffe424143b512d194bd31022e5
sylabe <sebastien.thibaud@gmail.com>
2014-03-19 16:42:35 +0100

    Update follows.js

--- follows.js
+++ follows.js
@@ -150,7 +150,7 @@
 										height: 200,
 										renderer: 'bar',
 										min: parseFloat(datastream.min_value),
-										max: parseFloat(datastream.current_value)*1.01,
+										max: parseFloat(datastream.max_value),
 										padding: {
 											top: 0.02,
 											right: 0.02,
